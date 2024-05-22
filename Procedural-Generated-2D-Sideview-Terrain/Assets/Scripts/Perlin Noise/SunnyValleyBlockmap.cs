eeb6e46042199ea2b0d243eb6db6ee88f3dbf256
Michel Jouaux <mjouaux@stud.hs-heilbron.de>
2024-05-22 20:50:14 +0200

    Add: ErenCode's Perlin Noise and Sunny Valley's Perlin Noise

---
+++ Procedural-Generated-2D-Sideview-Terrain/Assets/Scripts/Perlin Noise/SunnyValleyBlockmap.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class SunnyValleyBlockmap : MonoBehaviour
+{
+    public TileBase dirtGrass, dirtTile, stoneGrass, stoneTile, perlin2D, stoneDark, dirtDark;
+}
